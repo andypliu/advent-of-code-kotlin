8fdb87e6f6188fd345b222fab0b093dcf2de0f6e
Andy Liu <andyp.liu@gmail.com>
2023-12-04 13:13:36 -0800

    Add template and sample test cases for previous days

---
+++ src/DayXY.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val input = readInput("DayXYTest")
+    DayXY().part1(input)
+    DayXY().part2(input)
+}
+
+class DayXY {
+    fun part1(input: List<String>) {
+    }
+
+    fun part2(input: List<String>) {
+    }
+}
